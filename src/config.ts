ab4ae3697a312548493b959d1d0819dd19c41bab
qiuyue520 <qiuyue070212@qq.com>
2026-05-16 00:31:37 +0800

    再次修复无法部署。。。要崩溃了怎么那么麻烦

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export const SITE = {
   author: "xiaonai",
   description: "记录工程师 xiaonai 的不枯燥生活，每周一发布，欢迎关注",
   keywords: "xiaonai,Weekly,Blog,前端,linux,设计",
-  icon: "/icon-144.png",
-  siteImage: "/xnwky.png",
+  icon: "/Weekly/icon-144.png",
+  siteImage: "/Weekly/xnwky.png",
   homePage: "https://weekly.xiaonai.top",
   blogPage: "https://blog.xiaonai.top",
 };
